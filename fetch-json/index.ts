015106c67360513f9d076028cbf56186e21d0820
Steven Torrence <steve@cratebind.com>
2022-03-02 21:43:26 -0600

    Add interface and parameter types to the file

--- fetch-json/index.ts
+++ fetch-json/index.ts
@@ -2,6 +2,27 @@ import axios from 'axios';
 
 const url = 'http://jsonplaceholder.typicode.com/todos/1';
 
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
 axios.get(url).then(response => {
-  console.log(response.data);
+  const todo = response.data as Todo;
+
+  const id = todo.id
+  const title = todo.title;
+  const completed = todo.completed;
+
+  logTodo(id, title, completed);
+  
 });
+
+const logTodo = (id: number, title: string, completed: boolean) => {
+  console.log(`
+    The Todo with ID: ${id}
+    Has a title of: ${title}
+    Is it finished? ${completed}
+  `);
+}
